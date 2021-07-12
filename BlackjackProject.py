50528155d6da5dd52a6063ad0851d474d168116b
Tyler-Coombs <84094311+Tyler-Coombs@users.noreply.github.com>
2021-07-12 21:27:59 -0230

    Functioning Blackjack Program - Debugged
    Debugged but still needs exceptions.
    Fully functional when user enters correct values.

--- BlackjackProject.py
+++ BlackjackProject.py
@@ -7,11 +7,14 @@ import db #import module created to write/read money file
 
 print("BLACKJACK!")
 print("Blackjack payout is 3:2")
+print()
 
+#function to exit from the program 
 def exit_program():
     print("Exiting program. Bye!")
     sys.exit()
 
+#function to generate deck of cards as list of lists
 def deckOfCards():
     #sets the card types and values
     cardValue = ['Ace','2','3','4','5','6','7','8','9','10','J','Q','K']
@@ -23,205 +26,285 @@ def deckOfCards():
             deck.append(j + ' of ' + i)
     return deck
 
-def playerPointValue(playerCard):
+#function to get value of each card
+def pointValue(card):
     #only reading first slice to determine value of the card
-    if playerCard[:1] in ('J','Q','K','10'):
+    if card[:1] in ('J','Q','K'):
         return int(10)
-    elif playerCard[:1] in ('2','3','4','5','6','7','8','9'):
-        #card[:1] example '2' out of the full '2 of Hearts' string
-        return int(playerCard[:1])
-    elif playerCard[:1] == 'A':
-        print ("\n"+ str(playerCard))
-        num = input("Do you want this to be 1 or 11?\n>")
-        while num !='1' or num !='11':
-            if num == '1':
-                return int(1)
-            elif num == '11':
-                return int(11)
-            else:
-                num = input("Do you want this to be 1 or 11?\n>")
-
-def dealerPointValue(dealerCard):
-    #only reading first slice to determine value of the card
-    if dealerCard[:1] in ('J','Q','K','10'):
+    #checks for the card beginning with a 1
+    elif card[:1] == '1':
         return int(10)
-    elif dealerCard[:1] in ('2','3','4','5','6','7','8','9'):
+    elif card[:1] in ('2','3','4','5','6','7','8','9'):
         #card[:1] example '2' out of the full '2 of Hearts' string
-        return int(dealerCard[:1])
-    elif dealerCard[:1] == 'A':
-        print ("\n"+ str(dealerCard))
-        num = input("Do you want this to be 1 or 11?\n>")
-        while num !='1' or num !='11':
-            if num == '1':
-                return int(1)
-            elif num == '11':
-                return int(11)
+        return int(card[:1])
+    elif card[:1] == 'A':
+        return int(11)
+
+#function to deal a random card
+def dealCard(deck):
+    card = random.choice(deck)
+    return card
+
+#function to get player bet
+def playerBet(money):
+        while True:
+            betAmount = float(input("Bet Amount: "))
+            if betAmount < 5 or betAmount > 1000:
+                print("Invalid bet amount. Please bet between 5 and 1000.")
+                continue
+            elif betAmount > float(money[0]):
+                print("Invalid bet amount. You cannot bet more money than you have.")
+                continue
             else:
-                num = input("Do you want this to be 1 or 11?\n>")
+                newMoney = float(money[0]) - betAmount
+                money.clear()
+                money.append(newMoney)
+                db.writeMoney(money)
+                return betAmount
+                break
 
-#def dealerHand(deck):
-  #  dealerHand = []
-    #card = random.choice(deck)
-    #dealerHand.append(card)
-    #print(dealerHand)
-    #return dealerHand
-
-#def playerHand(deck):
-  #  playerHand = []
-   # card = random.choice(deck)
-   # playerHand.append(card)
-   # print(playerHand)
-   # return playerHand
-
-def hitOrStand():
-    playerChoice = "hit"
-    pass
-
-def dealerScore(dealerHand):
-    points = 0
-    for dealerCard in dealerHand:
-        points += dealerPointValue(dealerCard)
-        return points
-
-def playerScore(playerHand):
-    points = 0
-    for playerCard in playerHand:
-        points += playerPointValue(playerCard)
-        return points
-
-def playerBet():
-    money = db.readMoney()
-    print("Money: " + str(money[0]))
-    if float(money[0]) < 5:
-        buyChips = str(input("Not enough chips. Buy more? (y/n)"))
-        if buyChips.lower == "y":
-            buyAmount = float(input("How many chips would you like to buy? "))
-            money[0] = float(money[0]) + buyAmount
-            db.writeMoney(money)
-            return money
-        else:
-            print("You do not have enough chips to play.")
-            exit_program()
-    else:
-        betAmount = float(input("Bet Amount: "))
-        if betAmount < 5 or betAmount > 1000:
-            print("Invalid bet amount. Please bet between 5 and 1000.")
-        elif betAmount > float(money[0]):
-            print("Invalid bet amount. You cannot bet more money than you have.")
-        else:
-            money[0] = float(money[0]) - betAmount
-            #money.append(money[0])
-            db.writeMoney(money)
-            return betAmount
-
-def winnings():
-    betAmount = playerBet()
-    winAmount = betAmount * 1.5
-    money = db.readMoney()
-    money = money + winAmount
-    db.writeMoney(money)
-    print("Money: " + str(money))
-
-
+#gameplay function
 def playGame(deck):
     playerHand = []
     dealerHand = []
     playerScore = 0
     dealerScore = 0
+    winnings = 0
+    money = db.readMoney()
 
-    print("DEALER'S SHOW CARD")
-    dealerCard = random.choice(deck)
-    dealerHand.append(dealerCard)
-    deck.remove(dealerCard)
-    #card = dealerCard
-    dealerScore += dealerPointValue(dealerCard)
-    print(dealerHand)
-    dealerCard = random.choice(deck)
-    dealerHand.append(dealerCard)
-    deck.remove(dealerCard)
-    #card = dealerCard
-    dealerScore += dealerPointValue(dealerCard)
-    if len(dealerHand) == 2:
-        if dealerPointValue(dealerHand[0]) == 11 and dealerPointValue(dealerHand[1]) == 11:
-            dealerScore -= 10
-    
-    while len(playerHand) < 2:
-        playerCard = random.choice(deck)
-        playerHand.append(playerCard)
-        deck.remove(playerCard)
-        #card = playerCard
-        playerScore += playerPointValue(playerCard)
-        #playerScore = playerScore(playerHand)
-
-        if len(playerHand) == 2:
-            if playerPointValue(playerHand[0]) == 11 and playerPointValue(playerHand[1]) == 11:
-                playerScore -= 10
+    #prompt player to buy more chips if they have less than minimum bet in wallet
+    if float(money[0]) < 5:
+        print("Money: " + str(money[0]))
+        print("You need more money to play.")
+        buyChips = input("Would you like to add money to the wallet? (y/n)")
+        while buyChips.lower() == "y":
+            buyAmount = float(input("\nHow much money will you add to your wallet? Enter 0 to exit without adding to your wallet. >>"))
+            if buyAmount == 0:
+                break
+            elif buyAmount >= 5:
+                newMoney = float(money[0]) + buyAmount
+                money.clear()
+                money.append(newMoney)
+                db.writeMoney(money)
+                buyChips = input("Continue adding money? (y/n)")
+                print()
+            else:
+                print("You do not have enough money to play.")
+                exit_program()
         
-        print("YOUR CARDS: ")
-        print(playerHand)
+    if float(money[0]) >= 5:
+        print("Money: " + str(money[0]))
+        #get player bet
+        betAmount = playerBet(money)
 
-    while playerScore < 21:
-        choice = input("Hit or stand? (hit/stand): ")
-        if choice.lower() == "hit":
+        #deal player a card
+        card = dealCard(deck)
+        playerHand.append(card)
+        deck.remove(card)
+        points = pointValue(card)
+        playerScore += int(points)
+        #allow player to choose the value of A
+        if pointValue(card) == 11:
+            num = input("\nDo you want this Ace to be 1 or 11?\n>")
+            while num !='1' or num !='11':
+                if num == '1':
+                    playerScore -= 10
+                    break
+                elif num == '11':
+                    playerScore = playerScore
+                    break
+                else:
+                    print("You must choose 1 or 11")
+                    continue
 
-            playerCard = random.choice(deck)
-            playerHand.append(playerCard)
-            deck.remove(playerCard)
-            #card = playerCard
+        #deal dealer a card
+        card = dealCard(deck)
+        dealerHand.append(card)
+        deck.remove(card)
+        points = pointValue(card)
+        dealerScore += points
 
-            playerScore += playerPointValue(playerCard)
-            print(playerHand)
-            continue
+        #deal player second card
+        card = dealCard(deck)
+        playerHand.append(card)
+        deck.remove(card)
+        points = pointValue(card)
+        playerScore += points
+        #allow user to choose value of A
+        if pointValue(card) == 11:
+            num = input("Do you want this Ace to be 1 or 11?\n>")
+            while num !='1' or num !='11':
+                if num == '1':
+                    playerScore -= 10
+                    break
+                elif num == '11':
+                    playerScore = playerScore
+                    break
+                else:
+                    print("You must choose 1 or 11")
+                    continue
 
-        elif choice.lower() == "stand":
-            break
+        #deal dealer second card
+        card = dealCard(deck)
+        dealerHand.append(card)
+        deck.remove(card)
+        points = pointValue(card)
+        dealerScore += points
+        #if the user has more than one ace, one will only count as 1 point
+        if len(dealerHand) == 2:
+            if pointValue(dealerHand[0]) == 11 and pointValue(dealerHand[1]) == 11:
+                dealerScore -= 10
 
-        else:
-            print("Please enter hit or stand.")
-            continue
+        #show one of dealer's cards and players cards
+        print("\nDEALER'S SHOW CARD:")
+        print(dealerHand[0])
+        print("Hidden Card")
+        print()
+        print("YOUR CARDS:")
+        print("\n".join(playerHand))
+        print()
 
-    print("DEALER'S CARDS: ")
-    print(dealerHand)
+        #give player option to get dealt another card or to stick with what they have
+        while playerScore < 21:
+            choice = input("Hit or stand? (hit/stand): ")
+            print()
+            if choice.lower() == "hit":
+                card = dealCard(deck)
+                playerHand.append(card)
+                deck.remove(card)
+                points = pointValue(card)
+                playerScore += points
+                #allow player to choose the value of A
+                if pointValue(card) == 11:
+                    num = input("Do you want this Ace to be 1 or 11?\n>")
+                    print()
+                    while num !='1' or num !='11':
+                        if num == '1':
+                            playerScore -= 10
+                            break
+                        elif num == '11':
+                            playerScore = playerScore
+                            break
+                        else:
+                            print("You must choose 1 or 11")
+                            continue
+                print("YOUR CARDS")
+                print("\n".join(playerHand))
+                print()
+                if dealerScore < 17: #when player hits, dealer should also get another card if their score is <17
+                    card = dealCard(deck)
+                    dealerHand.append(card)
+                    deck.remove(card)
+                    points = pointValue(card)
+                    dealerScore += points
+                    #in the presence of another A, set the value of A to 1 for the dealer
+                    if len(dealerHand) >= 2 and pointValue(card) == 11:
+                        if pointValue(dealerHand[0]) == 11 or pointValue(dealerHand[1]) == 11:
+                            dealerScore -= 10
+                continue
+            elif choice.lower() == "stand":
+                if dealerScore < 17: #when player stands, dealer should get cards until their score is >17
+                    card = dealCard(deck)
+                    dealerHand.append(card)
+                    deck.remove(card)
+                    points = pointValue(card)
+                    dealerScore += points
+                    #in the presence of another A, set the value of an A to 1 for the dealer
+                    if len(dealerHand) >= 2 and pointValue(card) == 11:
+                        if pointValue(dealerHand[0]) == 11 or pointValue(dealerHand[1]) == 11 or pointValue(dealerHand[2]) == 11:
+                            dealerScore -= 10
+                break
+            else:
+                print("Please enter hit or stand.")
+                continue
 
-    print("YOUR POINTS:\t" + str(playerScore))
-    print("DEALER'S POINTS:\t" + str(dealerScore))
+        #display points and dealer's cards
+        print("DEALER'S CARDS")
+        print("\n".join(dealerHand))
+        print()
+        print("YOUR POINTS:          " + str(playerScore))
+        print("DEALER'S POINTS:   " + str(dealerScore))
+        print()
 
-    if playerScore == 21 and dealerScore < 21:
-        print("YOU HAVE A BLACKJACK!")
-        print("YOU WIN!")
-        winnings()
-    elif playerScore > dealerScore and playerScore < 21:
-        print("YOU WIN!")
-        winnings()
-    elif dealerScore > playerScore and dealerScore < 21:
-        print("Sorry. You lose.")
-        money = db.readMoney()
-        print("Money: " + str(money))
-    elif dealerScore == 21 and playerScore < 21:
-        print("Sorry. You lose.")
-        money = db.readMoney()
-        print("Money: " + str(money))
-    elif playerScore == 21 and dealerScore == 21:
-        print("Draw. No winners. You get your money back.")
-        
-    elif playerScore > 21:
-        print("BUST. Sorry. You lose.")
-        money = db.readMoney()
-        print("Money: " + str(money))
-        
+        #code to decide winner/loser and payout
+        if playerScore == 21 and dealerScore < 21:
+            print("YOU HAVE A BLACKJACK!")
+            print("YOU WIN!")
+            winnings = betAmount * 1.5
+            money = db.readMoney()
+            newMoney = float(money[0]) + winnings
+            money.clear()
+            money.append(newMoney)
+            db.writeMoney(money)
+            print("Money: " + str(money[0]))
+        elif playerScore == 21 and dealerScore > 21:
+            print("YOU HAVE A BLACKJACK!")
+            print("YOU WIN!")
+            winnings = betAmount * 1.5
+            money = db.readMoney()
+            newMoney = float(money[0]) + winnings
+            money.clear()
+            money.append(newMoney)
+            db.writeMoney(money)
+            print("Money: " + str(money[0]))
+        elif playerScore > dealerScore and playerScore < 21:
+            print("YOU WIN!")
+            winnings = betAmount * 1.5
+            money = db.readMoney()
+            newMoney = float(money[0]) + winnings
+            money.clear()
+            money.append(newMoney)
+            db.writeMoney(money)
+            print("Money: " + str(money[0]))
+        elif (playerScore < dealerScore and playerScore != 21) and dealerScore > 21:
+            print("DEALER BUSTS!")
+            print("YOU WIN!")
+            winnings = betAmount * 1.5
+            money = db.readMoney()
+            newMoney = float(money[0]) + winnings
+            money.clear()
+            money.append(newMoney)
+            db.writeMoney(money)
+            print("Money: " + str(money[0]))
+        elif dealerScore > playerScore and dealerScore < 21:
+            print("Sorry. You lose.")
+            money = db.readMoney()
+            print("Money: " + str(money[0]))
+        elif dealerScore == 21 and playerScore < 21:
+            print("Dealer has Blackjack.")
+            print("Sorry. You lose.")
+            money = db.readMoney()
+            print("Money: " + str(money[0]))
+        elif playerScore == 21 and dealerScore == 21:
+            print("Draw. No winners.")
+            winnings = betAmount
+            money = db.readMoney()
+            newMoney = float(money[0]) + winnings
+            money.clear()
+            money.append(newMoney)
+            db.writeMoney(money)
+            print("Money: " + str(money[0]))
+        elif playerScore == dealerScore:
+            print("Draw. No winners.")
+            winnings = betAmount
+            money = db.readMoney()
+            newMoney = float(money[0]) + winnings
+            money.clear()
+            money.append(newMoney)
+            db.writeMoney(money)
+            print("Money: " + str(money[0]))
+        elif playerScore > 21:
+            print("BUST. Sorry. You lose.")
+            money = db.readMoney()
+            print("Money: " + str(money[0]))
 
 def main():
     playAgain = "y"
     deck = deckOfCards()
     while playAgain.lower() == "y":
-
-        playerBet()
         playGame(deck)
-
-
-        playAgain = input("Play again? (y/n): ")
-        print()
-    print("Come back soon! \nBye!")
+        playAgain = input("\nPlay again? (y/n): ")
+    print("\nCome back soon! \nBye!")
 
 
 if __name__ == "__main__":
